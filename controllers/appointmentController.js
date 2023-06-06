3857ee8bb791bb53d2126b0cea8b2b51ad1ad480
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-06-06 09:42:07 +0100

    valid appointement

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,4 +1,3 @@
-
 import User from "../models/User.js";
 import Appointment from "../models/Appointment.js";
 
@@ -16,21 +15,70 @@ const addAppointment = async (req, res) => {
       newAppointment
     });
   } catch (error) {
-    console.log("OOP",error);
+    console.log("OOP", error);
     res.status(400).json({
       msg: "ERROR ADD"
     });
   }
 };
-const getAll = async(req, res)=>{
-    try {
-        const appointment = await Appointment.find().populate("posted_by");
-        return res.status(200).json({ appointment})
-        
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({msg:"ERROR GET ALL"})
-    }
-}
 
-export { addAppointment , getAll};
+const getAllAppointment = async (req, res) => {
+  try {
+    const appointments = await Appointment.find().populate("posted_by");
+    res.status(200).json({ appointments });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "ERROR GET ALL" });
+  }
+};
+
+const getAppointmentByUserId = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const appointments = await Appointment.find({ UserId: userId });
+    res.status(200).json({ appointments });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "ERROR GET BY USER ID" });
+  }
+};
+
+const updateAppointment = async (req, res) => {
+  try {
+    const appointmentId = req.params.id;
+    const updatedAppointment = await Appointment.findByIdAndUpdate(
+      appointmentId,
+      {
+        dateProg: req.body.dateProg,
+        name: req.body.name,
+        phone: req.body.phone,
+        address: req.body.address,
+        commercial: req.body.commercial
+      },
+      { new: true }
+    );
+    res.status(200).json({ updatedAppointment });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "ERROR UPDATE" });
+  }
+};
+
+const deleteAppointment = async (req, res) => {
+  try {
+    const appointmentId = req.params.id;
+    await Appointment.findByIdAndDelete(appointmentId);
+    res.status(200).json({ msg: "Appointment deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "ERROR DELETE" });
+  }
+};
+
+export {
+  addAppointment,
+  getAllAppointment,
+  getAppointmentByUserId,
+  updateAppointment,
+  deleteAppointment
+};
--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { addAppointment, getAll } from '../controllers/appointmentController.js'
+import { addAppointment, deleteAppointment, getAllAppointment, getAppointmentByUserId, updateAppointment } from '../controllers/appointmentController.js'
 import verifyAuth  from '../middleware/verifyAuth.js'
 
 const router = express.Router()
 
 router.route('/add-new').post(verifyAuth, addAppointment)
-router.route('/getAll').get(verifyAuth, getAll)
+router.route('/').get(verifyAuth, getAllAppointment)
+router.route('/:id').get(verifyAuth, getAppointmentByUserId)
+router.route('/:id').put(verifyAuth, updateAppointment)
+router.route('/:id').delete(verifyAuth, deleteAppointment)
 
 
 export default router
