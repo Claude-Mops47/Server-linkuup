c0d6cae80fff7061c44a6ead3eee157bb15da559
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-06-22 11:24:31 +0100

    rest

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -8,18 +8,16 @@ import {
   updateAppointment,
 } from "../controllers/appointmentController.js";
 import verifyAuth from "../middleware/verifyAuth.js";
-import { setETagHeader } from "../middleware/utils.js";
+// import { setETagHeader } from "../middleware/utils.js";
 
 const router = express.Router();
 
 router.route("/add-new").post(verifyAuth, addAppointment);
-router.route("/").get(verifyAuth, setETagHeader, getAllAppointment);
+router.route("/").get(verifyAuth, getAllAppointment);
 
-router
-  .route("/user/:id")
-  .get(verifyAuth, setETagHeader, getAppointmentByUserId);
+router.route("/user/:id").get(verifyAuth, getAppointmentByUserId);
 
-router.route("/:id").get(verifyAuth, setETagHeader, getAppointmentById);
+router.route("/:id").get(verifyAuth, getAppointmentById);
 router.route("/:id").put(verifyAuth, updateAppointment);
 router.route("/:id").delete(verifyAuth, deleteAppointment);
 
