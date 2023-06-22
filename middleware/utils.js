3a18bda1fc5272f319223bdf7a82a7e3a9ed5300
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-06-22 10:49:34 +0100

    version ok

--- middleware/utils.js
+++ middleware/utils.js
@@ -1,7 +1,31 @@
-export const generateVersion = (appointments) => {
+import Appointment from "../models/Appointment.js";
+
+let appointmentsVersion = null;
+
+const generateVersion = (appointments) => {
   let version = 0;
   for (const appointment of appointments) {
     version += appointment.version || 0;
   }
   return version.toString();
+};
+
+// Fonction pour récupérer tous les rdv
+const getAllAppointments = async () => {
+  return await Appointment.find().populate("posted_by");
+};
+
+// Fonction pour générer la version des rdv
+const generateAppointmentsVersion = async () => {
+  const appointments = await getAllAppointments();
+  return generateVersion(appointments);
+};
+
+// Middleware pour définir l'en-tête Etag
+export const setETagHeader = async (req, res, next) => {
+  if (!appointmentsVersion) {
+    appointmentsVersion = await generateAppointmentsVersion();
+  }
+  res.setHeader("ETag", appointmentsVersion);
+  next();
 };
--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -8,15 +8,19 @@ import {
   updateAppointment,
 } from "../controllers/appointmentController.js";
 import verifyAuth from "../middleware/verifyAuth.js";
+import { setETagHeader } from "../middleware/utils.js";
 
 const router = express.Router();
 
 router.route("/add-new").post(verifyAuth, addAppointment);
-router.route("/").get(verifyAuth, getAllAppointment);
-router.route("/user/:id").get(verifyAuth, getAppointmentByUserId);
-router.route("/:id").get(verifyAuth, getAppointmentById);
+router.route("/").get(verifyAuth, setETagHeader, getAllAppointment);
 
-router.route("/:id").put(verifyAuth, updateAppointment);
-router.route("/:id").delete(verifyAuth, deleteAppointment);
+router
+  .route("/user/:id")
+  .get(verifyAuth, setETagHeader, getAppointmentByUserId);
+
+router.route("/:id").get(verifyAuth, setETagHeader, getAppointmentById);
+router.route("/:id").put(verifyAuth, setETagHeader, updateAppointment);
+router.route("/:id").delete(verifyAuth, setETagHeader, deleteAppointment);
 
 export default router;
