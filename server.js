851084b85970b419f10c7955e2d6dd3818b64157
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-06-30 10:40:14 +0100

    ttt

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(cookieParser())
+app.use(cookieParser('secret_key'))
 
 app.use(helmet());
 
