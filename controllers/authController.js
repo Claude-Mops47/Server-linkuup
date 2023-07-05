e284c248607d6906e2660dddec3c9d8e3a49bdf3
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-07-05 13:37:09 +0100

    yy

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,8 +83,8 @@ const logout = async (req,res)=>{
 }
 // refres token
 const refreshAccessToken = (req,res)=>{
-  const refreshToken = req.cookie.refreshToken;
-  // const refreshToken = req.headers["X-Refresh-Token"];
+  // const refreshToken = req.cookie.refreshToken;
+  const refreshToken = req.headers["x-refresh-token"];
   console.log('refresh=',refreshToken);
 
   if(!refreshToken){
